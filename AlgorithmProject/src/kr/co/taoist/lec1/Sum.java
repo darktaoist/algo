0e0ad9d42d085b206b659eac80c0291258dfffe9
taoist <taoist@DESKTOP-N9EGVTR>
2017-11-01 21:59:54 +0900

    sdf

---
+++ AlgorithmProject/src/kr/co/taoist/lec1/Sum.java
@@ -0,0 +1,15 @@
+package kr.co.taoist.lec1;
+
+import java.util.Scanner;
+
+public class Sum {
+
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		int a , b;
+		a = sc.nextInt();
+		b = sc.nextInt();
+		System.out.println("a+b = " + (a+b));
+	}
+	
+}
