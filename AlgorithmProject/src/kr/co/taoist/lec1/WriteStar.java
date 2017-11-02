9e7eb5923426b2f35f9ddd22921ffd36c31ce245
taoist <taoist@DESKTOP-N9EGVTR>
2017-11-02 22:37:49 +0900

    44

--- AlgorithmProject/src/kr/co/taoist/lec1/WriteStar.java
+++ AlgorithmProject/src/kr/co/taoist/lec1/WriteStar.java
@@ -8,40 +8,23 @@ public class WriteStar {
 		int num = sc.nextInt();
 		int arr[] = new int[num];
 		int x = 1;
+		
 		for (int i = 1; i <= num; i++) {
 			arr[i-1] = x;
 			x+=2;
 		}
 		
 		int max = arr[arr.length-1];
-		
 		System.out.println(max);
-		for (int i=0;i<arr.length;i++) {
-			int mk = max / arr[i];
-			int mx = max % arr[i];
-			if(arr[i]==1) mx = max;
-			if (mx==0 && arr[i]!=max) {
-				for (int xx=0;xx<mk;xx++) {
-					System.out.print(" ");
-				}
-				for (int y=0;y<arr[i];y++) {
-					System.out.print("*");
-				}
-				System.out.println("");
-			} else {
-				int mmax = mx / 2;
-				for (int xxx=0; xxx < mmax; xxx++) {
-					System.out.print(" ");
-				}
-				
-				for (int y=0;y<arr[i];y++) {
-					System.out.print("*");
-				}
-				System.out.println("");
+		for (int i=0;i<arr.length;i++){
+			int m = (max-arr[i]) /2;
+			for (int j=0;j<m;j++) {
+				System.out.print(" ");
 			}
-			
-			//System.out.println(arr[i]);
+			for (int y=0;y<arr[i];y++) {
+				System.out.print("*");
+			}
+			System.out.println("");
 		}
-		
 	}
 }
